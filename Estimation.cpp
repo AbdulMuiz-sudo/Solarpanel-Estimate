19036c5fc0d428097565b2a201bd601d78a74851
Abdul Muiz Mazhar <l242501@lhr.nu.edu.pk>
2024-11-10 17:02:19 +0000

    second commit

--- Estimation.cpp
+++ Estimation.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
+	int panels=0;
+	float power=0.0;
 
-
+	cout<<"You require " << panels << " panels for a total usage of " << power << " Watts.\n";
 	return 0;
 }
